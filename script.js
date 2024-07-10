91e7d533fa27bbdcef89601aafea5877a4af044f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-10 08:27:53 +0530

    selected hints and attempts

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let attempts = 0;
 let randomNum = Math.floor(Math.random() * 100 + 1);
 
+const hint = document.querySelector("#hint");
+const attemptText = document.querySelector("#attempts")
 const guess = document.querySelector("#guess");
 const submit = document.querySelector("#submit");
